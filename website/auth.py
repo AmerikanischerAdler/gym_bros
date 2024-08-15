3e3c768c532ce0862408b278c8913fc63772aabb
AmerikanischerAdler <114196294+AmerikanischerAdler@users.noreply.github.com>
2024-08-15 12:26:17 -0400

    deleted unused page and created page for content creation

--- website/auth.py
+++ website/auth.py
@@ -2,17 +2,9 @@ from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
-@auth.route('/sign')
-def sign():
-    return render_template('sign.html')
-
 @auth.route('/login')
 def login():
     return render_template('login.html')
-
-@auth.route('/sign-up')
-def sign_up():
-    return render_template('sign_up.html')
 
 @auth.route('/logout')
 def logout():
