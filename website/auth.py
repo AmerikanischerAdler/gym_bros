3a053e0b8de5b6c99ad3228aaac67129a136e744
AmerikanischerAdler <114196294+AmerikanischerAdler@users.noreply.github.com>
2024-08-15 13:08:29 -0400

    prepared login form for backend connection

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 
 auth = Blueprint("auth", __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    name = request.form.get("name")
+    username = request.form.get("username")
+    email = request.form.get("email")
+    password = request.form.get("password")
     return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
-    return render_template('logout.html')
+    return redirect(url_for("pages.home"))
 
 
--- website/pages.py
+++ website/pages.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 
 pages = Blueprint("pages", __name__)
 
