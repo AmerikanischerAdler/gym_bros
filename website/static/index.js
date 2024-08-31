e9e14356f6554e4fe7bfa1a20e17287caac49aa7
AmerikanischerAdler <114196294+AmerikanischerAdler@users.noreply.github.com>
2024-08-30 22:58:42 -0400

    updated dynamic liking and unliking from home page and saved

--- website/static/index.js
+++ website/static/index.js
@@ -2,22 +2,39 @@ function like(postId) {
     const likeCount = document.getElementById(`likes-count-${postId}`);
     const likeButton = document.getElementById(`like-button-${postId}`);
     const postElement = document.getElementById(`post-${postId}`);
+    const postContainer = document.getElementById('post-container');
+    const titleDiv = document.getElementById('title');
 
-    fetch(`/like-post/${postId}`, {method: "POST"})
+    fetch(`/like-post/${postId}`, { method: "POST" })
         .then((res) => res.json())
         .then((data) => {
             if (data.error) {
                 alert(data.error);
             } else {
-                if (!data.liked) {
-                    postElement.remove();
-                } else {
-                    // Like Count
-                    likeCount.textContent = data.likes;
 
-                    // Colors If Liked/Not Liked
+                // Like Count
+                likeCount.textContent = data.likes;
+
+                // Color Change
+                if (data.liked) {
                     likeButton.classList.remove("not_liked_post");
                     likeButton.classList.add("liked_post");
+                } else {
+                    likeButton.classList.remove("liked_post");
+                    likeButton.classList.add("not_liked_post");
+
+                    // Remove from Saved
+                    if (postElement && window.location.pathname === "/saved") {
+                        postElement.remove();
+
+                        if (postContainer.children.length === 0) {
+                            titleDiv.innerHTML = `
+                                <h1>Saved</h1>
+                                <h2>It looks like you don't have any saved posts...</h2>
+                                <h4>Click the heart to save your favorite posts!</h4>
+                            `;
+                        }
+                    }
                 }
             }
         })
@@ -25,3 +42,4 @@ function like(postId) {
             console.error("Error:", error);
         });
 }
+
