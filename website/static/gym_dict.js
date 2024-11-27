102e1b5a4d15968fed0168a76dc524c58bfdf7af
AmerikanischerAdler <114196294+AmerikanischerAdler@users.noreply.github.com>
2024-11-27 16:42:05 -0500

    book works!!!!!!

--- website/static/gym_dict.js
+++ website/static/gym_dict.js
@@ -1,24 +1,35 @@
-var frontCover = document.getElementById("front-cover");
-
 document.addEventListener("DOMContentLoaded", function () {
     const frontCover = document.getElementById("front-cover");
+    const book = document.querySelector(".book");
 
-    frontCover.addEventListener("animationend", () => {
-        if (frontCover.classList.contains("flipped")) {
-            frontCover.style.zIndex = 1; // Behind the book after flipping
-        } else {
-            frontCover.style.zIndex = 3; // On top before flipping
-        }
-    });
-
-    frontCover.addEventListener("click", () => {
+    function coverTurn() {
         if (!frontCover.classList.contains("flipped")) {
             frontCover.style.animation = "open-cover 1s forwards";
             frontCover.classList.add("flipped");
+
+            $("#front-cover").css("z-index", "1");
+            $("#cover-container").css("z-index", "1");
+            $(".book").css("z-index", "50");
+            $(".book-contain").css("z-index", "50");
+
+            $(".book").css("visibility", "visible");
+            $(".book-contain").css("visibility", "visible");
         } else {
-            frontCover.style.animation = "close-cover 1s forwards"; // Define a reverse animation
+            frontCover.style.animation = "close-cover 1s forwards";
             frontCover.classList.remove("flipped");
+
+            $("#front-cover").css("z-index", "100");
+            $("#cover-container").css("z-index", "100");
+            $(".book").css("z-index", "50");
+            $(".book-contain").css("z-index", "50");
+
+            $(".book").css("visibility", "hidden");
+            $(".book-contain").css("visibility", "hidden");
         }
-    });
+    }
+
+    frontCover.addEventListener("click", coverTurn);
+    //book.addEventListener("click", coverTurn);
 });
+
 
