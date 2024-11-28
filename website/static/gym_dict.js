ca3b2683ed45177a2a10c60e276bd1fed6666495
AmerikanischerAdler <114196294+AmerikanischerAdler@users.noreply.github.com>
2024-11-28 11:00:07 -0500

    added book description box and 3d pages

--- website/static/gym_dict.js
+++ website/static/gym_dict.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const frontCover = document.getElementById("front-cover");
     const book = document.querySelector(".book");
+    const info = document.querySelector(".info-container");
 
     function coverTurn() {
         if (!frontCover.classList.contains("flipped")) {
             frontCover.style.animation = "open-cover 1s forwards";
             frontCover.classList.add("flipped");
+
+            info.style.display = "none";
 
             $("#front-cover").css("z-index", "1");
             $("#cover-container").css("z-index", "1");
@@ -17,6 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
             frontCover.style.animation = "close-cover 1s forwards";
             frontCover.classList.remove("flipped");
+
+            info.style.display = "flex";
 
             $("#front-cover").css("z-index", "100");
             $("#cover-container").css("z-index", "100");
